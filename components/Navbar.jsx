94b32ef6f78299346fc7efdc5eadd8abeb4df6c7
Avidu <avidu97@gmail.com>
2022-01-27 11:41:56 +0530

    basic menu added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import styles from '../styles/Navbar.module.css';
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 
 const Navbar = () => {
-  return <div className={styles.mainnav}>this is the navbar</div>;
+  return <div className={styles.mainnav}>
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger className={styles.trigger}>MENU</DropdownMenu.Trigger>
+      <DropdownMenu.Content className={styles.content}>
+        <DropdownMenu.Item className={styles.items}>PRICES</DropdownMenu.Item>
+        <DropdownMenu.Item className={styles.items}>PROJECTS</DropdownMenu.Item>
+        <DropdownMenu.Item className={styles.items}>CONTACT</DropdownMenu.Item>
+        <DropdownMenu.Item className={styles.items}>WE</DropdownMenu.Item>
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
+  </div>;
 };
 
 export default Navbar;
