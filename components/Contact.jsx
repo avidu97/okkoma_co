f949cfc1d21439afd9faec64141e40bab2c9c56a
Avidu <avidu97@gmail.com>
2022-01-28 08:31:03 +0530

    styling contact

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,20 +1,21 @@
 import styles from "../styles/Contact.module.css";
 import React from "react";
+import Link from "next/link";
 
 const Contact = () => {
   return (
     <div>
       <h2>SAY Hi!</h2>
-      <div>
-        <div>
-          <h1>hi@okkoma.co</h1>
+      <div className={styles.contact_container}>
+        <div className={styles.email_wrapper}>
+          <a href={"mailto:hi@okkoma.co"}><h1 className={styles.mail} >hi@okkoma.co</h1></a>
         </div>
-        <div>
-          <ul>
+        <div className={styles.bottom_sec}>
+          <ul className={styles.pn_wrapper}>
             <li>071 75 79 79 0</li>
             <li>076 75 78 12 7</li>
           </ul>
-          <address>
+          <address className={styles.adrs}>
             Kurunegala raod, <br />
             Kumbukgete, <br />
             60508.
